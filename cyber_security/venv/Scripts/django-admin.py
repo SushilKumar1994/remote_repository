c155eeea629e6ee8f74157ab0c875fff7d69ab04
sushilkumarshinde <sushilkumarshinde888@gmail.com>
2019-05-30 02:52:39 +0530

    firstcommit

---
+++ cyber_security/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Nit\Desktop\projects 2019\cyber_security\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
